854809deb7b51c8e1862475617be14f50c049003
joshua&elaiza <cheepcode@gmail.com>
2023-12-13 01:58:58 +0800

    Add_change_by_joshua

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 function navigateTo(page) {
    
-    const contentSections = document.querySelectorAll('main');
+    const contentSections = document.querySelectorAll('section');
+
     contentSections.forEach(section => {
         section.style.display = 'none';
     });
@@ -12,3 +13,43 @@ function navigateTo(page) {
 
 
 navigateTo('home');
+
+document.addEventListener('DOMContentLoaded', function () {
+   
+    const openButton = document.getElementById('openjoshuaButton');
+    const openElizaButton = document.getElementById('openElizaButton');
+
+    const hiddenMottoE ="The good news about computers is that they do what you tell them to do. The bad news is that they do what you tell them to do." ;
+    const hiddenMotto ="The advance of technology is based on making it fit in so that you don't really even notice it, so it's part of everyday life.";
+
+   
+    openButton.addEventListener('click', function () {
+        
+        alert(hiddenMotto);
+    });
+
+  
+    openElizaButton.addEventListener('click', function () {
+      
+        alert(hiddenMottoE);
+    });
+});
+function showJoshuaResume() {
+    var joshuaResume = document.getElementById('joshua-resume');
+    var elizaResume = document.getElementById('eliza-resume');
+
+    if (joshuaResume && elizaResume) {
+        joshuaResume.style.display = 'block';
+        elizaResume.style.display = 'none';
+    }
+}
+
+function showElizaResume() {
+    var joshuaResume = document.getElementById('joshua-resume');
+    var elizaResume = document.getElementById('eliza-resume');
+
+    if (joshuaResume && elizaResume) {
+        joshuaResume.style.display = 'none';
+        elizaResume.style.display = 'block';
+    }
+}
